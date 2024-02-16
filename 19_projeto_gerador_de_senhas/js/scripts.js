c06361e8a5a15310ca5883077072fb6f5fdd3385
Luiz Fillipe <78454639+LuizFillipe1@users.noreply.github.com>
2024-02-16 14:52:42 -0300

    Gerador de Senhas

---
+++ 19_projeto_gerador_de_senhas/js/scripts.js
@@ -0,0 +1,17 @@
+// Seleção de elementos
+
+const generatedPasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Funções
+// Letras, números e símbolos
+
+const getLetterLowerCose = () => {
+    console.log(String.fromCharCode('-'))
+}
+
+getLetterLowerCose();
+// Eventos
+generatedPasswordButton.addEventListener("click", () => {
+    console.log("testes");
+})
